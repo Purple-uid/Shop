cb3d7942331d94c22cdad8539c88e342b82f02a5
Gadis <dio30435@gmail.com>
2026-03-28 18:28:39 +0300

    zustand: Cart.tsx

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,39 +1,27 @@
-import { useState, useEffect } from 'react'
-import type  { CartItem } from '../../types/types'
-import { FaTrashAlt } from "react-icons/fa";
+import { useCartStore } from '../../store/cartStore'
+import { FaTrashAlt } from "react-icons/fa"
 import './Cart.css'
 
 function Cart() {
-    const [ cart, setCart ] = useState<CartItem[]>([])
-
-    useEffect(() => {
-        const saved = JSON.parse(localStorage.getItem('cart') || '[]')
-        setCart(saved)
-    }, [])
-
-    const addDelete = (id: number) => {
-        const newCart = cart.filter((prev) => prev.id !== id)
-        setCart(newCart)
-        localStorage.setItem('cart', JSON.stringify(newCart))
-    }
-    
+    const { cart, removeItem } = useCartStore()
 
     return (
         <div className='mainCart'>
             <h1>Корзина ваших товаров</h1>
-        {cart.map(c =>(
-            <div className='cart' key={c.id}>
-                <div className='Img'>
-                    <img src={c.image} alt={c.title} />
+            {cart.map(c => (
+                <div className='cart' key={c.id}>
+                    <div className='Img'>
+                        <img src={c.image} alt={c.title} />
+                    </div>
+                    <div className='cartData'>
+                        <h3 className='cartTitle'>{c.title}</h3>
+                        <h3 className='cartPrice'>{c.price} $</h3>
+                        <button onClick={() => removeItem(c.id)} className='cartBTN'>
+                            <FaTrashAlt />
+                        </button>
+                    </div>
                 </div>
-                
-                <div className='cartData'>
-                    <h3 className='cartTitle'>{c.title}</h3>
-                    <h3 className='cartPrice'>{c.price} $</h3>
-                    <button onClick={() => addDelete(c.id)} className='cartBTN'><FaTrashAlt /></button>
-                </div>
-            </div>
-        ))}
+            ))}
         </div>
     )
 }
